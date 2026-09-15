82dd3003af93fde3f886e829b0220cb52581577a
Santi Rivera (laptop) <santirivera92@gmail.com>
2026-09-15 18:24:11 +0200

    Hola Mundo

---
+++ script.js
@@ -0,0 +1,6 @@
+const greeting = document.getElementById('greeting');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  greeting.textContent = '¡Hola! soy Santi';
+});
